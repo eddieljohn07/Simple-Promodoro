f4725fd407da5809b235a525f2f9e920b2f6c287
eddieljohn07 <100287870+eddieljohn07@users.noreply.github.com>
2023-04-13 17:44:27 +0800

    add debounce util func

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ let minutes = 25;
 let seconds = 0;
 let isPomodoro = true;
 let isRunning = false;
+
+const debounce = (func, delay) => {
+    let timeoutId;
+    return (...args) => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        func(...args);
+      }, delay);
+    };
+  };
